e12a9192228b791834617b2f7f7f3c10279d349a
zwp <hello@zwp.cn>
2025-04-24 15:34:01 +0800

    MybatisPlus分页配置

---
+++ src/main/java/com/zwp/speeddating/config/MybatisPlusConfig.java
@@ -0,0 +1,26 @@
+package com.zwp.speeddating.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MybatisPlus 配置
+ */
+@Configuration
+@MapperScan("com.zwp.speeddating.mapper")
+public class MybatisPlusConfig {
+
+    /**
+     * 新的分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
