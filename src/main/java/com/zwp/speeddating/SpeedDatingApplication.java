5896824652d2e98650351d42c4099a41861a0807
zwp <hello@zwp.cn>
2025-04-24 14:22:58 +0800

    开启spring对定时任务的支持

--- src/main/java/com/zwp/speeddating/SpeedDatingApplication.java
+++ src/main/java/com/zwp/speeddating/SpeedDatingApplication.java
@@ -3,9 +3,11 @@ package com.zwp.speeddating;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("com.zwp.speeddating.mapper")
+@EnableScheduling // 开启spring对定时任务的支持
 public class SpeedDatingApplication {
 
     public static void main(String[] args) {
