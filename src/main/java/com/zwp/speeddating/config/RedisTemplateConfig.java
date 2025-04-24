476494791ef7c8d5a8044c2b04ff4f4a92ab90c3
zwp <hello@zwp.cn>
2025-04-24 16:01:13 +0800

    配置 RedisTemplate

--- src/main/java/com/zwp/speeddating/config/RedisTemplateConfig.java
+++ src/main/java/com/zwp/speeddating/config/RedisTemplateConfig.java
@@ -18,7 +18,7 @@ public class RedisTemplateConfig {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         // 设置连接工厂：将传入的 redisConnectionFactory 设置到 redisTemplate 中，以便它可以通过该工厂与 Redis 服务器建立连接。
         redisTemplate.setConnectionFactory(redisConnectionFactory);
-        // 设置键序列化器：指定键的序列化方式为字符串（RedisSerializer.string()）。这意味着在存储和读取键时会使用字符串格式。
+        // 设置键序列化器：指定键的序列化方式为字符串RedisSerializer.string(),这意味着在存储和读取键时会使用字符串格式。
         redisTemplate.setKeySerializer(RedisSerializer.string());
         return redisTemplate;
     }
