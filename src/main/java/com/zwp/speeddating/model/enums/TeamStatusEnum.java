aeb2dfe96cea845173d6b14b2f7cafd0cf56e871
zwp <hello@zwp.cn>
2025-04-25 16:10:29 +0800

    队伍状态枚举

---
+++ src/main/java/com/zwp/speeddating/model/enums/TeamStatusEnum.java
@@ -0,0 +1,54 @@
+package com.zwp.speeddating.model.enums;
+
+/**
+ * 队伍状态枚举
+ */
+public enum TeamStatusEnum {
+
+    PUBLIC(0, "公开"),
+    PRIVATE(1, "私有"),
+    SECRET(2, "加密");
+
+    private int value;
+
+    private String text;
+
+    /**
+     * 根据枚举值得到状态枚举
+     * @param value
+     * @return
+     */
+    public static TeamStatusEnum getEnumByValue(Integer value) { // Integer可以传递 null 值
+        if (value == null) {
+            return null;
+        }
+        TeamStatusEnum[] values = TeamStatusEnum.values();
+        for (TeamStatusEnum teamStatusEnum : values) {
+            if (teamStatusEnum.value == value) {
+                return teamStatusEnum;
+            }
+        }
+        return null;
+    }
+
+    TeamStatusEnum(int value, String text) {
+        this.value = value;
+        this.text = text;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
