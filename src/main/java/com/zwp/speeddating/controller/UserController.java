3ccb3a3c2033040d788207a6b1e66b58c09ab195
zwp <hello@zwp.cn>
2025-04-26 16:35:17 +0800

    匹配用户开发

--- src/main/java/com/zwp/speeddating/controller/UserController.java
+++ src/main/java/com/zwp/speeddating/controller/UserController.java
@@ -183,4 +183,20 @@ public class UserController {
         int result = userService.updateUser(user, loginUser);
         return ResultUtils.success(result);
     }
+
+    /**
+     * 获取最匹配的用户
+     * @param num
+     * @param request
+     * @return
+     */
+    @GetMapping("/match")
+    public BaseResponse<List<User>> matchUsers(long num, HttpServletRequest request) {
+        if (num <= 0 || num > 20) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        User loginUser = userService.getLoginUser(request);
+        List<User> userList = userService.matchUsers(num, loginUser);
+        return ResultUtils.success(userList);
+    }
 }
